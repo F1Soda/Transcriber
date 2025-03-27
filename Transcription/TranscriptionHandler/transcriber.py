77f20e79a44a39492faae65d5743bb3578d858ca
F1Soda <golik.tima@gmail.com>
2025-03-27 14:39:42 +0500

    Very good result with small errors on bound of chunk

--- Transcription/TranscriptionHandler/transcriber.py
+++ Transcription/TranscriptionHandler/transcriber.py
@@ -4,7 +4,7 @@ import re
 import torch
 import soundfile as sf
 from transformers import WhisperForConditionalGeneration, WhisperProcessor, pipeline
-from Audio.AudioHandler.convertor import Convertor
+from Audio.AudioHandler.preprocessor import Preprocessor
 
 class Transcriber:
     torch_dtype = torch.bfloat16
@@ -35,7 +35,7 @@ class Transcriber:
         model=whisper,
         tokenizer=processor.tokenizer,
         feature_extractor=processor.feature_extractor,
-        chunk_length_s=Convertor.chunk_duration,
+        chunk_length_s=Preprocessor.chunk_duration,
         stride_length_s=0,
         batch_size=8,
         torch_dtype=torch_dtype
@@ -56,58 +56,79 @@ class Transcriber:
 
         ITER_COUNT = 20
 
-        for k in range(math.ceil(len(chunk_files) / ITER_COUNT)):
-            waveforms = []
-            filenames = []
+        # for k in range(math.ceil(len(chunk_files) / ITER_COUNT)):
+        #     waveforms = []
+        #     filenames = []
+        #
+        #     for u in range(ITER_COUNT*k, min(ITER_COUNT*k + ITER_COUNT, len(chunk_files))):
+        #         fname = chunk_files[u]
+        #         path = os.path.join(chunks_folder, fname)
+        #         waveform, sample_rate = sf.read(path)
+        #
+        #         if sample_rate != 16000:
+        #             raise ValueError(f"Whisper требует 16kHz, но у файла {sample_rate}Hz. Переконвертируйте!")
+        #
+        #         waveforms.append(waveform)
+        #         filenames.append(fname)
+        speech_timestamps = []
+        with open('D:/PycharmProjects/Findex/Audio/Segments/20-02.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                start, end = line.split()
+                speech_timestamps.append({'start': float(start), 'end': float(end)})
 
-            for u in range(ITER_COUNT*k, min(ITER_COUNT*k + ITER_COUNT, len(chunk_files))):
-                fname = chunk_files[u]
-                path = os.path.join(chunks_folder, fname)
-                waveform, sample_rate = sf.read(path)
+        waveforms = []
+        with sf.SoundFile('D:/PycharmProjects/Findex/merged10m.wav') as f:
+            for pair in speech_timestamps:
+                start_time = pair['start']
+                end_time = pair['end']
 
-                if sample_rate != 16000:
-                    raise ValueError(f"Whisper требует 16kHz, но у файла {sample_rate}Hz. Переконвертируйте!")
+                sample_rate = f.samplerate
+                start_frame = int(start_time * sample_rate)
+                end_frame = int(end_time * sample_rate)
 
+                f.seek(start_frame)
+                frames_to_read = end_frame - start_frame
+                waveform = f.read(frames_to_read)
                 waveforms.append(waveform)
-                filenames.append(fname)
 
-            results = Transcriber.asr_pipeline(
-                waveforms,
-                generate_kwargs={"language": "russian"},
-                return_timestamps="sentence"
-            )
+        results = Transcriber.asr_pipeline(
+            waveforms,
+            generate_kwargs={"language": "russian"},
+            return_timestamps="sentence"
+        )
 
-            # Transcribe and save
-            with open(output_path, "w", encoding="utf-8") as out_file:
-                for idx, (asr, filename) in enumerate(zip(results, filenames)):
-                    audio_path = os.path.join(chunks_folder, filename)
+        # Transcribe and save
+        with open(output_path, "w", encoding="utf-8") as out_file:
+            for idx, (asr, pair) in enumerate(zip(results,speech_timestamps)):
+                #audio_path = os.path.join(chunks_folder, filename)
 
-                    correct_idx = int(re.search(r'part(\d+)', os.path.basename(filename)).group(1))
+                #correct_idx = int(re.search(r'part(\d+)', os.path.basename(filename)).group(1))
 
-                    print(f"\nProcessing: {filename}")
+                #print(f"\nProcessing: {filename}")
 
-                    waveform, sample_rate = sf.read(audio_path)
-                    if sample_rate != 16000:
-                        raise ValueError(f"Whisper требует 16kHz, но у файла {sample_rate}Hz. Переконвертируйте!")
+                #waveform, sample_rate = sf.read(audio_path)
+                #if sample_rate != 16000:
+                #    raise ValueError(f"Whisper требует 16kHz, но у файла {sample_rate}Hz. Переконвертируйте!")
 
-                    time_offset = correct_idx * Convertor.chunk_duration
+                time_offset = pair['start']
 
-                    for chunk in asr["chunks"]:
-                        timestamp = chunk.get("timestamp")
-                        text = chunk.get("text", "").strip()
+                for chunk in asr["chunks"]:
+                    timestamp = chunk.get("timestamp")
+                    text = chunk.get("text", "").strip()
 
-                        if not text:
-                            continue
-                        if timestamp is None or timestamp[0] is None:
-                            timestamp = [0]
+                    if not text:
+                        continue
+                    if timestamp is None or timestamp[0] is None:
+                        timestamp = [0]
 
-                        timestamp_sec = timestamp[0] + time_offset
-                        minutes = int(timestamp_sec // 60)
-                        seconds = int(timestamp_sec % 60)
-                        timestamp_str = f"[{minutes:02}:{seconds:02}]"
-                        chunk_number = '{:>3}'.format(idx)
-                        res = f"{chunk_number} {timestamp_str} {text}\n"
-                        print(res, end="")
-                        out_file.write(res)
+                    timestamp_sec = timestamp[0] + time_offset
+                    minutes = int(timestamp_sec // 60)
+                    seconds = int(timestamp_sec % 60)
+                    timestamp_str = f"[{minutes:02}:{seconds:02}]"
+                    chunk_number = '{:>3}'.format(idx)
+                    res = f"{chunk_number} {timestamp_str} {text}\n"
+                    print(res, end="")
+                    out_file.write(res)
 
         print(f"Transcript saved to: {output_path}")
--- VAD.py
+++ VAD.py
@@ -2,6 +2,7 @@
 from pydub import AudioSegment
 import torch
 from silero_vad import load_silero_vad, get_speech_timestamps
+import re
 
 # 1. Directory with audio files
 input_dir = 'D:/PycharmProjects/Findex/Audio/ProcessedAudios/20-02'
@@ -9,37 +10,44 @@ input_dir = 'D:/PycharmProjects/Findex/Audio/ProcessedAudios/20-02'
 # 2. Merge all audio files in memory using pydub
 combined = AudioSegment.empty()
 
-for filename in sorted(os.listdir(input_dir)):
+chunk_files = {int(re.search(r'part(\d+)', os.path.basename(f)).group(1)): f for f in os.listdir(input_dir)}
+chunk_files = [value for key, value in sorted(chunk_files.items())]
+
+for filename in chunk_files:
     if filename.endswith('.mp3') or filename.endswith('.wav'):
         audio_path = os.path.join(input_dir, filename)
         print(f"Loading {audio_path}...")
         audio = AudioSegment.from_file(audio_path)
         audio = audio.set_channels(1).set_frame_rate(16000)  # Ensure mono, 16kHz
         combined += audio
+
+combined.export("D:/PycharmProjects/Findex/merged10m.wav", format="wav")
 #
-# # 3. Convert to numpy -> torch tensor for Silero VAD
-# samples = combined.get_array_of_samples()
-# samples_tensor = torch.tensor(samples, dtype=torch.float32) / 32768.0  # Normalize 16-bit PCM
-# samples_tensor = samples_tensor.unsqueeze(0)  # Add batch dimension: shape [1, num_samples]
-#
-# # 4. Run Silero VAD
-# model = load_silero_vad()
-# speech_timestamps = get_speech_timestamps(samples_tensor, model, return_seconds=True)
-#
-# # 5. Save results
-# output_txt_path = 'D:/PycharmProjects/Findex/Audio/Segments/20-02.txt'
-# with open(output_txt_path, 'w') as file:
-#     for pair in speech_timestamps:
-#         file.write(f"{pair['start']} {pair['end']}\n")
-#
-# print(f"VAD results saved to: {output_txt_path}")
+# 3. Convert to numpy -> torch tensor for Silero VAD
+samples = combined.get_array_of_samples()
+samples_tensor = torch.tensor(samples, dtype=torch.float32) / 32768.0  # Normalize 16-bit PCM
+samples_tensor = samples_tensor.unsqueeze(0)  # Add batch dimension: shape [1, num_samples]
+
+# 4. Run Silero VAD
+model = load_silero_vad()
+speech_timestamps = get_speech_timestamps(samples_tensor, model, return_seconds=True,
+                                          threshold=0.3,
+                                          min_speech_duration_ms=1000,
+                                          min_silence_duration_ms=1000,)
+
+# 5. Save results
+output_txt_path = 'D:/PycharmProjects/Findex/Audio/Segments/20-02.txt'
+with open(output_txt_path, 'w') as file:
+    for pair in speech_timestamps:
+        file.write(f"{pair['start']} {pair['end']}\n")
+
+print(f"VAD results saved to: {output_txt_path}")
 speech_timestamps = []
 with open('D:/PycharmProjects/Findex/Audio/Segments/20-02.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
         start, end = line.split()
         speech_timestamps.append({'start': float(start), 'end': float(end)})
-
 
 speech_audio = AudioSegment.empty()
 
@@ -50,8 +58,8 @@ for i, ts in enumerate(speech_timestamps):
     speech_audio += segment
     print(f"Added speech segment {i}: {ts['start']}s - {ts['end']}s")
 
-output_path ='D:/PycharmProjects/Findex/no_silence.wav'
-
-# --- Step 5: Export new audio without silence ---
+output_path = 'D:/PycharmProjects/Findex/no_silence_v2.wav'
+#
+# # --- Step 5: Export new audio without silence ---
 speech_audio.export(output_path, format='wav')
-print(f"\n🎉 Cleaned audio saved to: {output_path}")
+# print(f"\n🎉 Cleaned audio saved to: {output_path}")
